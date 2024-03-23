8d1109a831538cb3b5762b2439998016dbb64c08
JacindaTang <jacindatang@163.com>
2024-03-23 23:12:54 +0800

    240323 新增toogle组件并完善切换主题功能

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,26 +2,13 @@ import React from "react";
 import { NavLink, Link } from 'react-router-dom';
 
 import logo from '@/assets/images/logo.png';
+import ThemeToogle from '@/components/ThemeToogle/ThemeToogle'
 
 import './NavBar.scss'
 
 class NavBar extends React.Component {
     render() {
         // let historyState = window.location.pathname; 获取当前路由地址
-
-        function changeTheme() {
-
-            const classList = document.documentElement.classList;
-            console.log(classList, '----classList')
-            if (classList.value) {
-                document.documentElement.classList.remove('dark');
-                return;
-            }
-            document.documentElement.classList.add('dark');
-        }
-
-
-
         return (
             <div className="home-nav flex-al just-bt">
                 <div className="home-nav-lt flex-al">
@@ -46,7 +33,7 @@ class NavBar extends React.Component {
                     </div>
                 </div>
                 <div className="home-nav-rt flex-al">
-                    <div onClick={changeTheme}>主题切换</div>
+                    <ThemeToogle></ThemeToogle>
                     <div className="login-btn">Login</div>
                 </div>
             </div>
