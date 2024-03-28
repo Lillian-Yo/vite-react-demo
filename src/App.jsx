a3e3074fa23c03d33d212aa6965b023e18c20d40
jacindatang@163.com <jacindatang@163.com>
2024-03-28 17:12:25 +0800

    240328 过渡动画修改

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,17 @@ import { Suspense, useEffect, useState } from 'react';
 import { useRoutes } from 'react-router-dom';
 
 import routers from './router';
-import LoadAnime from '@/components/Loading/HomeLoad/LoadAnime';
 
 import HomeLoad from './components/Loading/HomeLoad/HomeLoad';
 
 function App() {
   const element = useRoutes(routers);
-  const [load, setLoad] = useState(true);
+  const [load, setLoad] = useState(false);
 
   useEffect(() => {
+    setLoad(true)
     const timer = setTimeout(() => {
       setLoad(false)
-      LoadAnime();
     }, 6500);
 
     return () => clearTimeout(timer);
